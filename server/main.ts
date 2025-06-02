76b2ec44971c4a705508ad4c6c9990f868857079
John Sanders <jsanders@redviking.com>
2025-06-01 20:07:24 -0400

    build: dotenv

--- server/main.ts
+++ server/main.ts
@@ -1,13 +1,20 @@
 /// <reference lib="deno.ns" />
+import "@std/dotenv/load";
 import { serveDir } from "@std/http/file-server";
 
-Deno.serve((req) => {
+// const secret = Array.from(crypto.getRandomValues(new Uint8Array(16))).map(b => b.toString(16).padStart(2, "0")).join("");
+// console.log("Generated secret:", secret);
+
+Deno.serve({
+  port: parseInt(Deno.env.get("PORT") || "8000", 10),
+}, (req) => {
   const pathname = new URL(req.url).pathname;
   if (pathname.startsWith("/api/hello")) {
     return new Response("Hello, World!", {
       headers: { "Content-Type": "text/plain" },
     });
   }
+
   return serveDir(req, {
     fsRoot: "dist",
   });
